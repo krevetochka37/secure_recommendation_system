9619f3af3650faebac5ba7e573a239706ef29be1
krevetochka37 <131526183+krevetochka37@users.noreply.github.com>
2025-06-05 20:34:41 +0300

    Delete app directory

--- app/docker/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Запуск основного приложения
-echo "Starting the recommendation system..."
-python app/main.py &
-
-# Запуск веб-сервера для визуализации
-echo "Starting the web server for visualization..."
-nginx -g "daemon off;"
